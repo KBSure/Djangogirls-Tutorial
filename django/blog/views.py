245bbed44f96a128e1dc61a938dd11ee66d5aa8d
KBSure <byungsustory@gmail.com>
2018-01-30 15:31:54 +0900

    static template tag사용, blog.css구현

--- django/blog/views.py
+++ django/blog/views.py
@@ -11,7 +11,7 @@ def post_list(request):
     # 5. config.urls모듈은 ''(admin/를 제외한 모든 요청)을 blog.urls모듈로 전달
     # 6. blog.urls모듈은 받은 요청의 URL과 일치하는 패턴이 있는지 검사
     # 7. 있다면 일치하는 패턴과 연결된 함수(view)를 실행
-    #   7-1. settings모듈의 ㅆ뜨ㅖ밌ㄸㄴ속성 내의 DIR목록에서
+    #   7-1. settings모듈의 Templates 속성 내의 DIR목록에서
     #         blog/post_list.html파일의 내용을 가져옴
     #   7-2. 가져온 내용을 적절히 처리(렌더링, render()함수)하여 리턴
     # 8. 함수의 실행 결과(리턴값)를 브라우저로 다시 전달
