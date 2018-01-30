8f23bd6f77cdded4e34874962d224cda380990e8
KBSure <byungsustory@gmail.com>
2018-01-30 12:14:15 +0900

    templates/blog/post_list.htm파일 작성, blog.views.post_list에서 해당 파일 render한 결과 리턴

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def post_list(request):
@@ -9,7 +9,11 @@ def post_list(request):
     # 5. config.urls모듈은 ''(admin/를 제외한 모든 요청)을 blog.urls모듈로 전달
     # 6. blog.urls모듈은 받은 요청의 URL과 일치하는 패턴이 있는지 검사
     # 7. 있다면 일치하는 패턴과 연결된 함수(view)를 실행
+    # 7-1. settings모듈의 ㅆ뜨ㅖ밌ㄸㄴ속성 내의 DIR목록에서
+    #         blog/post_list.html파일의 내용을 가져옴
+    #   7-2. 가져온 내용을 적절히 처리(렌더링, render()함수)하여 리턴
     # 8. 함수의 실행 결과(리턴값)를 브라우저로 다시 전달
 
     # HTTP프로토콜로 텍스트 데이터 응답을 변환
-    return HttpResponse('<html><body><h1>Post list</h1><p>Post목록을 보여줄 예입니다.</p></body></html>')
+    # return HttpResponse('<html><body><h1>Post list</h1><p>Post목록을 보여줄 예입니다.</p></body></html>')
+    return render(request, 'blog/post_list.html')
